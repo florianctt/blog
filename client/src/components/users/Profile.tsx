a83d1ce38389f9b7eccbb9c9a61c9e228c0239d3
Florian Casterot <casterotflorian@gmail.com>
2022-11-16 00:01:41 +0100

    feat(admin): update user

--- client/src/components/users/Profile.tsx
+++ client/src/components/users/Profile.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { getUser } from '@/api/resolverUser'
-import { useRef } from 'react'
+import { globalStore } from '@/hooks/globalStore'
+import { useEffect, useRef } from 'react'
 
 export const Profile = () => {
-  const { status, data, error } = getUser()
+  const { status, error } = getUser()
+  const { dataState, setDataState } = globalStore.dataStore()
 
   const cardRef = useRef<HTMLDivElement>(null)
   const buttonRef = useRef<HTMLButtonElement>(null)
@@ -16,26 +18,74 @@ export const Profile = () => {
       : (buttonRef.current!.textContent = 'Modify')
   }
 
+  useEffect(() => {
+    dataState.length > 0 && console.log(dataState[0].firstName)
+  }, [dataState])
+
+  const updateUser = (value: string, input: string) => {
+    switch (input) {
+      case 'firstname':
+        dataState[0].firstName = value
+        return setDataState(dataState)
+      case 'lastname':
+        dataState[0].lastName = value
+        return setDataState(dataState)
+      case 'mobile':
+        dataState[0].mobile = value
+        return setDataState(dataState)
+      case 'email':
+        dataState[0].email = value
+        return setDataState(dataState)
+    }
+    // setDataState(dataState)
+  }
+
   return (
     <div className='users'>
       <h2>Profile</h2>
       <>
-        {status === 'loading' ? (
+        {dataState.length < 1 ? (
           <p>Loading...</p>
-        ) : status === 'error' ? (
-          <p>Error: {error instanceof Error && error.message}</p>
         ) : (
           <>
             <div ref={cardRef} className='card'>
               <div className='row'>
-                <input type='text' name='firstname' id='firstname' value={data.firstName} disabled />
-                <input type='text' name='lastname' id='lastname' value={data.lastName} disabled />
+                <input
+                  type='text'
+                  name='firstname'
+                  id='firstname'
+                  value={dataState[0].firstName}
+                  onChange={e => updateUser(e.target.value, 'firstname')}
+                  disabled
+                />
+                <input
+                  type='text'
+                  name='lastname'
+                  id='lastname'
+                  value={dataState[0].lastName}
+                  onChange={e => updateUser(e.target.value, 'lastname')}
+                  disabled
+                />
               </div>
               <div className='row'>
-                <input type='tel' name='mobile' id='mobile' value={data.mobile} disabled />
-                <input type='email' name='mail' id='mail' value={data.email} disabled />
+                <input
+                  type='tel'
+                  name='mobile'
+                  id='mobile'
+                  value={dataState[0].mobile}
+                  onChange={e => updateUser(e.target.value, 'mobile')}
+                  disabled
+                />
+                <input
+                  type='email'
+                  name='mail'
+                  id='mail'
+                  value={dataState[0].email}
+                  onChange={e => updateUser(e.target.value, 'email')}
+                  disabled
+                />
               </div>
-              {/* <input type='password' name='password' id='password' value={data.password} /> */}
+              {/* <input type='password' name='password' id='password' value={dataState[0].password} /> */}
             </div>
             <button ref={buttonRef} type='button' onClick={() => handleInput()}>
               Modify
